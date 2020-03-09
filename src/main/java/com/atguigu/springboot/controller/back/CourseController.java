13cf57295de58bdd0cd402bc37ab1b93d0e478a0
ZhouLaoRi <61609953+ZhouLaoRi@users.noreply.github.com>
2020-03-09 15:52:46 +0800

    增加用户界面模板html，以及课程增加条件查询

--- src/main/java/com/atguigu/springboot/controller/back/CourseController.java
+++ src/main/java/com/atguigu/springboot/controller/back/CourseController.java
@@ -5,6 +5,7 @@ import com.atguigu.springboot.entity.CourseExample;
 import com.atguigu.springboot.service.CourseService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -20,15 +21,64 @@ public class CourseController {
 
     //查询所有课程返回列表页面
     @GetMapping("/courses")
-    public String listCourse(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum,Model model){
+    public String listCourse(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum,Model model ,Course course){
 
+        CourseExample courseExample = new CourseExample();
+        if(course.getCourseId() != null || course.getCourseName() != null){
+            CourseExample.Criteria criteria = courseExample.createCriteria();
+            criteria.andCourseIdEqualTo(course.getCourseId());
+            criteria.andCourseNameEqualTo(course.getCourseName());
+        }
         PageHelper.startPage(pageNum, 5);
-        List<Course> courses = courseService.selectByExample(new CourseExample());
+        List<Course> courses = courseService.selectByExample(courseExample);
         PageInfo<Course> pageInfo = new PageInfo<Course>(courses);
         model.addAttribute("courses",courses);
         model.addAttribute("pageInfo",pageInfo);
         return "course/list";
     }
+
+    //查询所有课程返回列表页面
+    @PostMapping("/coursesAllBlog")
+    public String listCoursesAllBlog(@Param(value = "pageNum") Integer pageNum, Model model , Course course){
+
+        if(pageNum == null) pageNum = 1;
+
+        CourseExample courseExample = new CourseExample();
+        CourseExample.Criteria criteria = courseExample.createCriteria();
+        if(course.getCourseId() != null ){
+            criteria.andCourseIdEqualTo(course.getCourseId());
+        }
+        if(course.getCourseName() != null){
+            criteria.andCourseNameEqualTo(course.getCourseName());
+        }
+        PageHelper.startPage(pageNum, 5);
+        List<Course> courses = courseService.selectByExample(courseExample);
+        PageInfo<Course> pageInfo = new PageInfo<Course>(courses);
+        model.addAttribute("courses",courses);
+        model.addAttribute("pageInfo",pageInfo);
+        return "course/list :: CourseList";
+    }
+
+    //用来条件查询的 ：查询所有课程返回列表页面
+    @ResponseBody
+    @GetMapping("/coursesAll")
+    public PageInfo<Course> listAllCourse(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum ,Course course){
+
+        CourseExample courseExample = new CourseExample();
+        CourseExample.Criteria criteria = courseExample.createCriteria();
+        if(course.getCourseId() != null ){
+            criteria.andCourseIdEqualTo(course.getCourseId());
+        }
+        if(course.getCourseName() != null){
+            criteria.andCourseNameEqualTo(course.getCourseName());
+        }
+        PageHelper.startPage(pageNum, 5);
+        List<Course> courses = courseService.selectByExample(courseExample);
+        PageInfo<Course> pageInfo = new PageInfo<Course>(courses);
+        return pageInfo;
+    }
+
+
     //来到课程添加页面
     @RequestMapping("/course")
     public String addCourseGuide(Course course){
